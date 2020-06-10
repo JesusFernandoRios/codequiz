acc4783a50d214209d05e673f6f28ccea9246017
Jrios <Jesusfernandorios93@gmail.com>
2020-06-09 21:20:46 -0700

    updated answering function

--- main.js
+++ main.js
@@ -18,17 +18,68 @@ let questionContainer = document.querySelector(".question-container");
 let questionInsert = document.querySelector("#question");
 
 // Score
-let score = document.querySelector("#score")
+let scoreContainer = document.querySelector("#score")
 // this variable will input the live score of player
-let scoreCounter = document.querySelector("#score-counter")
+let scoreKeeper = document.querySelector("#score-counter")
+let progress = document.querySelector("#progress")
 
 
 start.addEventListener("click", startGame);
 
+
+
+// Questions Part
+let questions = [
+  {
+    question: "What is a Variable?",
+    box1: "a cascading style sheet",
+    box2: "a “named storage” for data.",
+    box3: "a number assigned to an equation",
+    correct: "2"
+    
+  },
+  {
+    question: 'In JavaScript what are Strings used for?',
+    box1: 'to perform different actions based on different conditions',
+    box2: 'to explain JavaScript code, and to make it more readable',
+    box3: 'storing and manipulating text',
+    correct: "3"
+  },
+  {
+    question: 'JavaScript programs can be inserted into any part of an HTML document with the help of which tag?',
+    box1: 'script',
+    box2: 'java',
+    box3: 'href',
+    correct: "1"
+    
+  },
+  {
+    question: 'The "if" statement evaluates a condition in parentheses and, if the result is ______, executes a block of code.',
+    box1: 'False',
+    box2: 'True', 
+    box3: 'Undefined',
+    correct: "2"
+  },
+  {
+    question: 'When did JavaScript start?',
+    box1: '1986',
+    box2: '1993',
+    box3: '1995',
+    correct: "3"
+  }
+]
+// variables for grabbing objects
+let lastQuestionIndex = questions.length - 1;
+let runningQuestionIndex = 0;
+let count = 0;
+
+
+
+
 function startGame() {
   startTimer();
-  runningQuestionIndex = 0
-  renderQuestion()
+  renderQuestion();
+  
   
   start.classList.add("hide");
   questionContainer.classList.remove("hide");
@@ -39,39 +90,42 @@ function startGame() {
 function renderQuestion(){
     let q = questions[runningQuestionIndex];
     questionInsert.innerHTML ="<p>" + q.question + "</p>"
-    box1.innerHTML = q.text1;
-    box2.innerHTML = q.text2;
-    box3.innerHTML = q.text3;
+    box1.innerHTML = q.box1;
+    box2.innerHTML = q.box2;
+    box3.innerHTML = q.box3;
     
 }
 
 
 
-
-
 function nextQuestion() {
     runningQuestionIndex++
-    renderQuestion()
+    renderQuestion();
+    
 
 }
 
 function checkAnswer(answer) {  
     if(questions[runningQuestionIndex].correct == answer){
-      scoreCounter ++;
-      answerIsCorrect();
-    }else {
-      answerIsWrong();
+      
+      count++
+      alert("Right Answer")
+    }
+    else{
+      alert("wrong answer")
     }
     if(runningQuestionIndex < lastQuestionIndex){
-      count = 0;
       runningQuestionIndex++;
       renderQuestion();
     }
     else{
       clearInterval(startTimer);
     }
+    
 
   }
+
+    
       
 
 
@@ -88,46 +142,6 @@ function startTimer() {
   }, 1000);
 }
 
-// Questions Part
-let questions = [
-  {
-    question: "What is a Variable?",
-    text1: "a cascading style sheet",
-    text2: "a “named storage” for data.",
-    text3: "a number assigned to an equation",
-    correct: "2"
-    
-  },
-  {
-    question: 'In JavaScript what are Strings used for?',
-    text1: 'to perform different actions based on different conditions',
-    text2: 'to explain JavaScript code, and to make it more readable',
-    text3: 'storing and manipulating text',
-    correct: "3"
-  },
-  {
-    question: 'JavaScript programs can be inserted into any part of an HTML document with the help of which tag?',
-    text1: '<script>',
-    text2: '<java>',
-    text3: '<href>',
-    correct: "1"
-    
-  },
-  {
-    question: 'The "if" statement evaluates a condition in parentheses and, if the result is ______, executes a block of code.',
-    text1: 'False',
-    text2: 'True', 
-    text3: 'Undefined',
-    correct: "2"
-  },
-  {
-    question: 'When did JavaScript start?',
-    text1: '1986',
-    text2: '1993',
-    text3: '1995',
-    correct: "3"
-  }
-]
-// variables for grabbing objects
-let lastQuestionIndex = questions.length - 1;
-let runningQuestionIndex = 0;
+function stopTimer() {
+  clearInterval(timer);
+}
